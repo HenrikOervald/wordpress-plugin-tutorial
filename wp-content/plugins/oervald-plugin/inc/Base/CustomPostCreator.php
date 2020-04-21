5e918c373562d425d63c3dbe481385394b37c246
unknown <henrik.oervald@gmail.com>
2020-04-21 15:46:52 +0200

    added save function for product type

--- wp-content/plugins/oervald-plugin/inc/Base/CustomPostCreator.php
+++ wp-content/plugins/oervald-plugin/inc/Base/CustomPostCreator.php
@@ -15,8 +15,22 @@ class CustomPostCreator{
      add_action('init', array($this, 'register_oervald_custom_post'));
 	 add_action('init', array($this, 'add_oervald_tax'));
 	 add_action('admin_init', array($this, 'admin_init'));
+	 add_action('save_post', array($this, 'save_details'));
 	 flush_rewrite_rules();
  }
+
+ function add_oervald_tax()
+ {
+	register_taxonomy("Product IDs", array("product"), array("hierarchical" => true, "label" => "Product IDs", "singular_label" => "Product ID", "rewrite" => true));
+ }
+
+ function admin_init()
+ {
+	 add_meta_box('product_id-meta','Product ID', array($this,'product_ID'),'product','side','low');
+	 add_meta_box("details_meta", "Design & Build Credits", array($this,"oervald_details_meta"), "product", "normal", "low");
+ }
+
+
 
  function register_oervald_custom_post()
  {
@@ -51,25 +65,40 @@ class CustomPostCreator{
 	register_post_type( 'product' , $args );
  }
 
- function add_oervald_tax()
- {
-	register_taxonomy("Product IDs", array("product"), array("hierarchical" => true, "label" => "Product IDs", "singular_label" => "Product ID", "rewrite" => true));
- }
-
- function admin_init()
- {
-	 add_meta_box('product_id-meta','Product ID', array($this,'product_ID'),'product','side','low');
- }
-
  function product_ID()
  {
 	 global $post;
 
 	 $custom = get_post_custom($post->ID);
-	 $product_description = $custom['product_description'][0];
+	 $product_description = $custom['product_ID'][0];
 	 ?>
-	 <label>Product Description</label>
-	 <input name="product_description" value="<?php echo $product_description; ?>"/>
-	 <?php
+		<label>Product Description</label>
+		<input name="product_ID" value="<?php echo $product_description; ?>" />
+<?php
  }
+
+ function oervald_details_meta() {
+	global $post;
+	$custom = get_post_custom($post->ID);
+	$designers = $custom["designers"][0];
+	$developers = $custom["developers"][0];
+	$producers = $custom["producers"][0];
+	?>
+		<p><label>Designed By:</label><br />
+    		<textarea cols="50" rows="5" name="designers"><?php echo $designers; ?></textarea></p>
+		<p><label>Built By:</label><br />
+    		<textarea cols="50" rows="5" name="developers"><?php echo $developers; ?></textarea></p>
+		<p><label>Produced By:</label><br />
+    		<textarea cols="50" rows="5" name="producers"><?php echo $producers; ?></textarea></p>
+<?php
+  }
+
+  function save_details(){
+	global $post;
+  
+	update_post_meta($post->ID, "product_ID", $_POST["product_ID"]);
+	update_post_meta($post->ID, "designers", $_POST["designers"]);
+	update_post_meta($post->ID, "developers", $_POST["developers"]);
+	update_post_meta($post->ID, "producers", $_POST["producers"]);
+  }
 }
